87cebb6c640da74be2c77c93ce689179531c6837
tigeryu8900 <tigeryu8900@gmail.com>
2026-04-23 09:14:39 -0700

    add Art Lapsa

---
+++ src/en/artlapsa/src/eu/kanade/tachiyomi/extension/en/artlapsa/ArtLapsa.kt
@@ -0,0 +1,87 @@
+package eu.kanade.tachiyomi.extension.en.artlapsa
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.util.asJsoup
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import uy.kohesive.injekt.injectLazy
+import kotlin.getValue
+
+class ArtLapsa : Keyoapp("Art Lapsa", "https://artlapsa.com", "en") {
+
+    private val json: Json by injectLazy()
+
+    override fun popularMangaParse(response: Response): MangasPage {
+        val mangas = super.popularMangaParse(response).mangas.distinctBy { it.url }
+        return MangasPage(mangas, false)
+    }
+
+    override fun genresRequest() = GET("$baseUrl/search", headers)
+
+    override fun parseGenres(document: Document): List<Genre> = document.select("[wire:model.live=genre] option:not(:contains(All))").map { Genre(it.text(), it.attr("value")) }
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val url = "$baseUrl/search".toHttpUrl().newBuilder().apply {
+            if (page > 1) addQueryParameter("page", page.toString())
+            if (query.isNotBlank()) addQueryParameter("title", query)
+            (filters.firstOrNull { it is GenreList } as? GenreList)?.state
+                ?.filter { it.state }
+                ?.forEach { addQueryParameter("genre", it.id) }
+        }.build()
+        return GET(url, headers)
+    }
+
+    override fun searchMangaSelector() = "main#main-content [wire:key*='serie']"
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        runCatching { fetchGenres() }
+        val document = response.asJsoup()
+        val mangas = document.select(searchMangaSelector()).map(::searchMangaFromElement)
+        return MangasPage(mangas, hasNextPage = mangas.size >= 20)
+    }
+
+    override val descriptionSelector = "#expand_content"
+    override val statusSelector = "[alt=Status]"
+    override val typeSelector = "[alt=Type]"
+
+    override fun chapterListSelector(): String {
+        if (!preferences.showPaidChapters) {
+            return "#chapters > a:not(:has(.text-sm span:matches(Upcoming))):not(:has(img[src*=star-circle]))"
+        }
+        return "#chapters > a:not(:has(.text-sm span:matches(Upcoming)))"
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        val data = json.parseToJsonElement(document.selectFirst("script[type=\"application/ld+json\"]")!!.data()).jsonObject
+        val seriesURL = data["isPartOf"]!!.jsonObject["url"]!!.jsonPrimitive.content
+        val series = seriesURL.substring(seriesURL.lastIndexOf('/') + 1)
+        val chapterURL = data["url"]!!.jsonPrimitive.content
+        val chapter = chapterURL.substring(chapterURL.lastIndexOf('/') + 1)
+        val numberOfPages = data["numberOfPages"]!!.jsonPrimitive.int
+        return (1..numberOfPages).mapIndexed { i, page ->
+            Page(
+                i,
+                document.location(),
+                "$baseUrl/storage/series/webtoon/$series/chapters/$chapter/${page.toString().padStart(3, '0')}.jpg",
+            )
+        }
+    }
+}
+
+private val spaces = Regex("\\s")
+private val pagesRegex = Regex("pages:(\\[[^]]+])")
+private val linkRegex = """baseLink:(["'])(.+?)\1""".toRegex()
+
+@Serializable
+class Path(val path: String)
